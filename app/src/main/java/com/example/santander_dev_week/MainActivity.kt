e17893d752b855f29985d51c9faef3ca2b4b66af
nailson13b <nailson.oliveira@outlook.com>
2021-06-01 21:53:19 -0300

    first commit

---
+++ app/src/main/java/com/example/santander_dev_week/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.santander_dev_week
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_1 -> {
+                Log.d("CLICK", "Click no item 1")
+                true
+            }
+            R.id.item_2 -> {
+                Log.d("CLICK", "Click no item 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
